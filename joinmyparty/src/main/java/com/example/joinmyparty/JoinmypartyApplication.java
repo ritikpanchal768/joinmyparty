6f397776a6502cf00cb69ce78b3c4d978ac7b98b
Ritik Panchal <ritikpanchal@Ritiks-MacBook-Air.local>
2026-01-07 13:13:37 +0530

    spring initialised

---
+++ joinmyparty/src/main/java/com/example/joinmyparty/JoinmypartyApplication.java
@@ -0,0 +1,13 @@
+package com.example.joinmyparty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinmypartyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinmypartyApplication.class, args);
+	}
+
+}
